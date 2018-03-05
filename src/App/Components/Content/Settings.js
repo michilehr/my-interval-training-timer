53801db3ed0e95f34f86a290ebcbdac2176bb7c3
Michi Lehr <mail@michilehr.de>
2018-03-05 17:37:18 +0100

    feature: disables auto complete for input settings

--- src/App/Components/Content/Settings.js
+++ src/App/Components/Content/Settings.js
@@ -32,15 +32,15 @@ class Settings extends React.Component {
                 <form>
                     <div className={"setting" + (this.state.currentSettingStep === 0 ? " current" : "")}>
                         <label>Timer</label>
-                        <input type="number" name="settingTimer" value={settingTimer} onChange={handleChangeSetting} />
+                        <input type="number" name="settingTimer" autoComplete="off" value={settingTimer} onChange={handleChangeSetting} />
                     </div>
                     <div className={"setting" + (this.state.currentSettingStep === 1 ? " current" : "")}>
                         <label>Sets</label>
-                        <input type="number" name="settingSets" value={settingSets} onChange={handleChangeSetting} />
+                        <input type="number" name="settingSets" autoComplete="off" value={settingSets} onChange={handleChangeSetting} />
                     </div>
                     <div className={"setting" + (this.state.currentSettingStep === 2 ? " current" : "")}>
                         <label>Rest</label>
-                        <input type="number" name="settingRest" value={settingRest} onChange={handleChangeSetting} />
+                        <input type="number" name="settingRest" autoComplete="off" value={settingRest} onChange={handleChangeSetting} />
                     </div>
 
                 </form>
